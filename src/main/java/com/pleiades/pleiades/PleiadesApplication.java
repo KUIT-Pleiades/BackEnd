9a30ffd63705eda19bfa2bce52d49923ef3e0e2a
yuna <yjeong1217@gmail.com>
2025-01-01 21:46:15 +0900

    initial commit

---
+++ src/main/java/com/pleiades/pleiades/PleiadesApplication.java
@@ -0,0 +1,13 @@
+package com.pleiades.pleiades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PleiadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PleiadesApplication.class, args);
+	}
+
+}
