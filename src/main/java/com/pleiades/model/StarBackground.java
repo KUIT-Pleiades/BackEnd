12f9b20c1784866216c824c766e0716bb7442b83
yuna <yjeong1217@gmail.com>
2025-01-09 14:11:17 +0900

    git: dev, 1-login-kakao conflict 해결

---
+++ src/main/java/com/pleiades/model/StarBackground.java
@@ -0,0 +1,14 @@
+package com.pleiades.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class StarBackground {
+    private int id;
+    private String name;
+    private String url;
+}
