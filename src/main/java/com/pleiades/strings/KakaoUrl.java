161afe9e261ec7761a908932e491ca5ab39adbf9
yuna <yjeong1217@gmail.com>
2025-01-16 23:03:23 +0900

    chore: KakaoUrl redirect url 수정2

--- src/main/java/com/pleiades/strings/KakaoUrl.java
+++ src/main/java/com/pleiades/strings/KakaoUrl.java
@@ -2,7 +2,7 @@ package com.pleiades.strings;
 
 public enum KakaoUrl {
     AUTH_URL("https://kauth.kakao.com/oauth/authorize"),
-    REDIRECT_URI("http://54.252.108.194//auth/login/kakao/callback"),
+    REDIRECT_URI("http://54.252.108.194/auth/login/kakao/callback"),
     TOKEN_URL("https://kauth.kakao.com/oauth/token"),
     USER_INFO_URL("https://kapi.kakao.com/v2/user/me"),
     ACCESS_TOKEN_INFO_URL("https://kapi.kakao.com/v1/user/access_token_info"),
