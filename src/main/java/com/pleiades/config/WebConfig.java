c76a2204f7068c3610bb49d5b6f54440b3001ecf
yuna <yjeong1217@gmail.com>
2025-01-19 17:54:26 +0900

    chore: cors config origin 추가

--- src/main/java/com/pleiades/config/WebConfig.java
+++ src/main/java/com/pleiades/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.pleiades.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${FRONT_ORIGIN}")
+    private String FRONT_ORIGIN;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해
-                .allowedOrigins("http://54.252.108.194")
+                .allowedOrigins("http://54.252.108.194", FRONT_ORIGIN)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
