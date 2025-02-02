e6a8dd994a7fb725707a6947e6754e4d46bf235a
lylylylh <yh81260@naver.com>
2025-02-02 15:53:03 +0900

    Refactor: naverLogin - refreshToken을 Cookie로

--- src/main/java/com/pleiades/controller/AuthNaverController.java
+++ src/main/java/com/pleiades/controller/AuthNaverController.java
@@ -8,6 +8,7 @@ import com.pleiades.service.NaverLoginService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,15 +17,15 @@ import java.io.IOException;
 
 @Slf4j
 @RestController
-@RequestMapping("/auth/login/naver")
+@RequestMapping("/auth/login")
 @RequiredArgsConstructor
 
 public class AuthNaverController {
 
     private final NaverLoginService naverLoginService;
 
-    @PostMapping("/callback")
-    public ResponseEntity<?> handleNaverLogin(@RequestBody NaverLoginRequest loginRequest) {
+    @PostMapping("/naver")
+    public ResponseEntity<?> handleNaverLogin(@RequestBody NaverLoginRequest loginRequest, HttpServletResponse response) {
         log.info("handleNaverLogin 시작");
 
         String authCode = loginRequest.getCode();
@@ -32,13 +33,24 @@ public class AuthNaverController {
         if (authCode == null) {
             log.error("에러: authCode -> NULL");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(new NaverLoginResponse("Invalid request", null));
+                    .body(null);
         }
         log.info("네이버 로그인 request 받음. AuthCode: {}", authCode);
-        LoginCBResponse response = naverLoginService.handleNaverLoginCallback(authCode);
-        return ResponseEntity.ok(response);
+        LoginCBResponse loginResponse = naverLoginService.handleNaverLoginCallback(authCode);
+        String accessToken = loginResponse.getAccessToken();
+        String refreshToken = loginResponse.getRefreshToken();
+
+        response.setHeader(HttpHeaders.SET_COOKIE, createRefreshTokenCookie(refreshToken));
+
+        return ResponseEntity.ok(accessToken);
     }
 
+    private String createRefreshTokenCookie(String refreshToken) {
+        int maxAge = 7 * 24 * 60 * 60;
+        boolean secure = false;
+        return String.format("refreshToken=%s; Path=/; Max-Age=%d; HttpOnly; SameSite=Lax; Secure=%s",
+                refreshToken, maxAge, secure ? "true" : "false");
+    }
     @ExceptionHandler(NaverRefreshTokenExpiredException.class)
     public ResponseEntity<Void> handleRefreshTokenExpired(HttpServletResponse response) throws IOException {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build(); //401
