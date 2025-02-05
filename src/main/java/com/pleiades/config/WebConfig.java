2fbaea81e81e7aec68685fe44673e5a0ea1be072
lylylylh <yh81260@naver.com>
2025-02-05 10:53:51 +0900

    log: naver 로그인 후 access token 출력

--- src/main/java/com/pleiades/config/WebConfig.java
+++ src/main/java/com/pleiades/config/WebConfig.java
@@ -36,7 +36,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(authInterceptor)
                 .addPathPatterns("/**")  // 모든 경로에 대해 인터셉터 적용
                 .excludePathPatterns("/auth/refresh",
-                        "auth/auth/refresh",
+                        "/auth/auth/refresh",
                         "/error",
                         "/auth/login/**",
                         "/auth/login/*",
--- src/main/java/com/pleiades/controller/AuthNaverController.java
+++ src/main/java/com/pleiades/controller/AuthNaverController.java
@@ -44,6 +44,7 @@ public class AuthNaverController {
 
         addRefreshTokenCookie(response, refreshToken);
 
+        log.info("네이버 로그인 access token: {}", accessToken);
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(Map.of("access_token", accessToken));
