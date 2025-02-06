7bab63f8135d986ea1f443f1b835b00defc86dc7
lylylylh <yh81260@naver.com>
2025-02-06 13:51:58 +0900

    Fix: local에서 star table 생성 O

--- src/main/java/com/pleiades/entity/Star.java
+++ src/main/java/com/pleiades/entity/Star.java
@@ -1,22 +1,20 @@
 package com.pleiades.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
-@Table(name = "stars")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@Table(name="stars")
+@Builder
 public class Star {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id; // User의 기본 키를 그대로 사용 (X)
+    private Long id; // User의 기본 키를 그대로 사용 (X)
 
     // @MapsId // User의 기본 키를 Star의 기본 키로 매핑
     @OneToOne(cascade = CascadeType.PERSIST)
