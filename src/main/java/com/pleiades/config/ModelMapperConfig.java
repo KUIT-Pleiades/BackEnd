9d4dffeb2dc92b8a69980423a77d3cbae3a4ef5a
yuna <yjeong1217@gmail.com>
2025-04-24 01:13:46 +0900

    fix: report title error

--- src/main/java/com/pleiades/config/ModelMapperConfig.java
+++ src/main/java/com/pleiades/config/ModelMapperConfig.java
@@ -1,5 +1,8 @@
 package com.pleiades.config;
 
+import com.pleiades.dto.ReportDto;
+import com.pleiades.dto.ReportListDto;
+import com.pleiades.entity.Report;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +19,22 @@ public class ModelMapperConfig {
                 .setFieldMatchingEnabled(true)
                 .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE);
 
+        reportToDto(modelMapper);
+
         return modelMapper;
+    }
+
+    private void reportToDto(ModelMapper modelMapper) {
+        modelMapper.typeMap(Report.class, ReportDto.class).addMappings(mapper -> {
+            mapper.map(Report::getId, ReportDto::setReportId);
+            mapper.map(src -> src.getQuestion().getId(), ReportDto::setQuestionId);
+            mapper.map(src -> src.getQuestion().getQuestion(), ReportDto::setQuestion);
+        });
+
+        modelMapper.typeMap(Report.class, ReportListDto.class).addMappings(mapper -> {
+            mapper.map(Report::getId, ReportListDto::setReportId);
+            mapper.map(src -> src.getQuestion().getId(), ReportListDto::setQuestionId);
+            mapper.map(src -> src.getQuestion().getQuestion(), ReportListDto::setQuestion);
+        });
     }
 }
