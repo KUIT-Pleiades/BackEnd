9c8ee0e18d2f4373f04b1ef9281f059fa4876e38
yuna <yjeong1217@gmail.com>
2025-07-25 22:53:55 +0800

    feat: Swagger - Authorization 헤더 추가

--- src/main/java/com/pleiades/config/SwaggerConfig.java
+++ src/main/java/com/pleiades/config/SwaggerConfig.java
@@ -1,11 +1,27 @@
 package com.pleiades.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@OpenAPIDefinition(
+        info = @io.swagger.v3.oas.annotations.info.Info(title = "API 문서", version = "v1"),
+        security = @SecurityRequirement(name = "Authorization") // 전역 보안 적용
+)
+@SecurityScheme(
+        name = "Authorization",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        in = SecuritySchemeIn.HEADER,
+        paramName = "Authorization" // Swagger UI에 입력 창 표시
+)
 @Configuration
 public class SwaggerConfig {
     @Bean
