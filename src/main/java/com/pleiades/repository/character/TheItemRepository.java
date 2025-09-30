500093e1cc0d5c9b41470575f0e0b286b2dfc7f6
yuna <yjeong1217@gmail.com>
2025-09-30 13:15:39 +0900

    fix: 공식몰 아이템 불러오기

--- src/main/java/com/pleiades/repository/character/TheItemRepository.java
+++ src/main/java/com/pleiades/repository/character/TheItemRepository.java
@@ -3,6 +3,7 @@ package com.pleiades.repository.character;
 import com.pleiades.entity.character.TheItem;
 import com.pleiades.strings.ItemType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,7 +16,8 @@ public interface TheItemRepository extends JpaRepository<TheItem, Long> {
     // @Query 없어도 동작
     List<TheItem> findByType(ItemType type);
 
-    List<TheItem> findByTypeIn(List<ItemType> types);
+    @Query("SELECT i FROM TheItem i WHERE i.type IN :types")
+    List<TheItem> findByTypes(List<ItemType> types);
 
     Optional<TheItem> findByTypeAndName(ItemType type, String name);
 
