f007b811659ab6c181d620d32665895ac57464c4
lylylylh <yh81260@naver.com>
2025-12-31 20:37:25 +0900

    feat: official store 검색 기능 추가

--- src/main/java/com/pleiades/repository/character/TheItemRepository.java
+++ src/main/java/com/pleiades/repository/character/TheItemRepository.java
@@ -23,4 +23,22 @@ public interface TheItemRepository extends JpaRepository<TheItem, Long> {
     Optional<TheItem> findByTypeAndName(ItemType type, String name);
 
     Optional<TheItem> findByName(String itemName);
+
+    // store 내 검색 query
+    @Query("""
+select distinct i
+from TheItem i
+left join i.itemThemes it
+left join it.theme t
+left join i.itemKeywords ik
+left join ik.keyword k
+where
+    (:query is null or :query = '' or
+     lower(i.name) like lower(concat('%', :query, '%'))
+     or lower(i.description) like lower(concat('%', :query, '%'))
+     or lower(t.name) like lower(concat('%', :query, '%'))
+     or lower(k.name) like lower(concat('%', :query, '%'))
+    )
+""")
+    List<TheItem> searchOfficialItems(@Param("query") String query);
 }
--- src/main/java/com/pleiades/repository/store/OfficialWishlistRepository.java
+++ src/main/java/com/pleiades/repository/store/OfficialWishlistRepository.java
@@ -14,10 +14,21 @@ import java.util.Optional;
 @Repository
 public interface OfficialWishlistRepository extends JpaRepository<OfficialWishlist, Long> {
 
+    // user ID, type (TOP, BOTTOM, SET ...) 으로 찾기
+    // TheItemRepository: item 찾을 때 type 으로 찾아야 함
     @Query("SELECT i FROM OfficialWishlist i WHERE i.item.type IN :types AND i.user.id=:userid")
     List<OfficialWishlist> findByTypesInWishlist(@Param("types") List<ItemType> types, @Param("userid") String userid);
 
     Optional<OfficialWishlist> findByUserIdAndItemId(String userId, Long itemId);
 
     boolean existsByUserIdAndItemId(String userId, Long itemId);
+
+    // store 내 검색 query
+    @Query("""
+select ow.item.id
+from OfficialWishlist ow
+where ow.user.id = :userId
+""")
+    List<Long> findAllWishlistItemIds(@Param("userId") String userId);
+
 }
