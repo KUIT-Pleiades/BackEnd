b3809ff651c45eeafffda27a366fe2a9f1234d71
yuna <yjeong1217@gmail.com>
2026-01-03 21:09:29 +0900

    fix: scheduler transactional

--- src/main/java/com/pleiades/scheduler/ResetUsersStoneCharge.java
+++ src/main/java/com/pleiades/scheduler/ResetUsersStoneCharge.java
@@ -4,12 +4,14 @@ import com.pleiades.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Component
 public class ResetUsersStoneCharge {
     private final UserRepository userRepository;
 
+    @Transactional
     @Scheduled(cron = "0 0 0 * * ?", zone = "Asia/Seoul") // 매일 자정(00:00:00)에 실행
     public void resetStoneCharge() {
         userRepository.resetStoneChargeToFalse();
